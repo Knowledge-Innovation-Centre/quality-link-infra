ae4edabbfa233b169fdb15aee1fe8e1b9f27c479
rokm <roky.mlakar@gmail.com>
2025-10-30 15:16:44 +0100

    build fix

--- 05_frontend/src/pages/DashboardPage.tsx
+++ 05_frontend/src/pages/DashboardPage.tsx
@@ -1,7 +1,6 @@
 import { useState, useEffect } from 'react'
 import { useParams, Link } from 'react-router-dom'
 import { providersApi } from '../api'
-import { API_CONFIG } from '../api/config'
 import type { GetProviderResponse, DatalakeFile } from '../types'
 import Navbar from '@/components/layout/Navbar'
 import Footer from '@/components/layout/Footer'
